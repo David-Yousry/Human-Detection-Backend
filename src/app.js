f51783b3e2492f35715c21865dc6037b087bc731
David_Yousry <davidyousry45@gmail.com>
2024-12-04 23:57:24 +0200

    CORS final fix

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:4200/",
+    origin: "http://localhost:4200",
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
   })
